10ed779dc593d8fca2ffde32c1cf787627e9860e
joethyr <joethayr@gmail.com>
2021-11-15 20:40:16 -0700

    added method that outputs the most common day in the event attendees csv file.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -56,6 +56,8 @@ end
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+
+
 def most_common_hour
   reg_hour_array = []
   contents.each do |row|
@@ -70,17 +72,31 @@ def most_common_hour
   most_common_hour.max_by { |_k, v| v }[0]
 end
 
-puts "\nThe most common hour of registration is: #{most_common_hour}:00"
-
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
-  phone_number = clean_phone_numbers(row[:homephone])
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(id, form_letter)
-
-  # puts "#{name} #{zipcode} #{phone_number}"
+def most_common_reg_day
+  reg_day_array = []
+  contents.each do |row|
+    reg_date = row[:regdate]
+    reg_day = Time.strptime(reg_date, '%M/%d/%y %k:%M').strftime('%A')
+    reg_day_array.push(reg_day)
+  end
+  most_common_day = reg_day_array.reduce(Hash.new(0)) do |hash, day|
+    hash[day] += 1
+    hash
+  end
+  most_common_day.max_by { |_k, v| v }[0]
 end
+
+puts "\nThe most common hour of registration is hour #{reg_date_time('%k')}:00."
+
+puts "\nThe most common day of registration is #{reg_date_time('%A')}."
+
+# contents.each do |row|
+#   id = row[0]
+#   name = row[:first_name]
+#   zipcode = clean_zipcode(row[:zipcode])
+#   legislators = legislators_by_zipcode(zipcode)
+#   phone_number = clean_phone_numbers(row[:homephone])
+#   form_letter = erb_template.result(binding)
+
+#   save_thank_you_letter(id, form_letter)
+# end
